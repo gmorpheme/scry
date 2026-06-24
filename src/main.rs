cdefe70759da8abaceefadb94936d140c1648135
Greg Hawkins <162815+gmorpheme@users.noreply.github.com>
2026-06-24 14:25:45 +0100

    Add --markdown output mode (#4)

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ pub mod annot;
 pub mod bundle;
 pub mod error;
 pub mod extract;
+pub mod markdown;
 pub mod options;
 pub mod rtf;
 pub mod scrivx;
@@ -23,6 +24,17 @@ fn main() {
     }
 }
 
+/// Infer project name from the scrivx file path
+fn project_name(project_file: &std::path::Path) -> String {
+    // Walk up to find the .scriv bundle directory
+    if let Some(parent) = project_file.parent() {
+        if let Some(name) = parent.file_stem() {
+            return name.to_string_lossy().to_string();
+        }
+    }
+    "project".to_string()
+}
+
 /// Run extraction capturing error for reporting
 fn try_main(opts: &options::Opt) -> Result<()> {
     let project_file = opts.project_file().ok_or(ScryError::CannotLocateScrivx)?;
@@ -31,12 +43,39 @@ fn try_main(opts: &options::Opt) -> Result<()> {
     let project = scrivx::ScrivenerProject::parse(scrivx)?;
     let bundle = bundle::Bundle::new(directory);
 
-    if opts.itemise() {
+    if opts.markdown() {
+        let output_dir = opts.output_dir().ok_or(ScryError::CannotLocateBundle)?;
+        let name = project_name(&project_file);
+        let folder_specs = opts.folder_specs();
+        let content_specs = opts.content_specs();
+
+        if opts.split() {
+            markdown::emit_split(
+                &project,
+                &bundle,
+                output_dir,
+                &name,
+                &folder_specs,
+                &content_specs,
+                opts.asset_path(),
+            )?;
+        } else {
+            markdown::emit_single_file(
+                &project,
+                &bundle,
+                output_dir,
+                &name,
+                &folder_specs,
+                &content_specs,
+                opts.asset_path(),
+            )?;
+        }
+    } else if opts.itemise() {
         let items = binder_iterator(&project, opts.folder_specs());
         let mut itemiser = JsonItemiser::new(opts.content_specs());
-        for item in items {
-            let folder = bundle.binder_item_content(&item.uuid);
-            itemiser.consume_item(item, &folder)?;
+        for item_location in items {
+            let folder = bundle.binder_item_content(&item_location.item.uuid);
+            itemiser.consume_item(&item_location, &folder)?;
         }
         itemiser.write_to_stdout()?;
     } else {
--- src/scrivx.rs
+++ src/scrivx.rs
@@ -2,6 +2,7 @@
 use quick_xml::de::{from_reader, DeError};
 use serde::{Deserialize, Deserializer};
 use std::{
+    collections::VecDeque,
     fmt,
     io::{BufReader, Read},
 };
@@ -10,20 +11,24 @@ use uuid::Uuid;
 /// Top level project element
 #[derive(Debug, Deserialize, PartialEq)]
 pub struct ScrivenerProject {
-    #[serde(rename = "Identifier", default)]
+    #[serde(rename = "@Identifier", default)]
     pub identifier: Uuid,
-    #[serde(rename = "Version", default)]
+    #[serde(rename = "@Version", default)]
     pub version: String,
-    #[serde(rename = "Creator", default)]
+    #[serde(rename = "@Creator", default)]
     pub creator: String,
-    #[serde(rename = "Device", default)]
+    #[serde(rename = "@Device", default)]
     pub device: String,
-    #[serde(rename = "Author", default)]
+    #[serde(rename = "@Author", default)]
     pub author: String,
+    #[serde(rename = "@Modified", default)]
+    pub modified: String,
+    #[serde(rename = "@ModID", default)]
+    pub mod_id: Uuid,
     #[serde(rename = "Binder")]
     pub binder: Binder,
-    #[serde(rename = "ModID")]
-    pub mod_id: Uuid,
+    #[serde(rename = "Collections")]
+    pub collections: Collections,
 }
 
 impl ScrivenerProject {
@@ -34,15 +39,15 @@ impl ScrivenerProject {
     }
 
     /// An iterator over all items in the project's binder
-    pub fn iter(&self) -> BinderIterator {
+    pub fn iter(&self) -> BinderIterator<'_> {
         BinderIterator::new(self.binder.binder_items.iter().collect())
     }
 
     /// Find the draft folder
     pub fn draft(&self) -> &BinderItem {
         for i in self.iter() {
-            if i.r#type == BinderItemType::DraftFolder {
-                return i;
+            if i.item.r#type == BinderItemType::DraftFolder {
+                return i.item;
             }
         }
         panic!("No draft folder in project!")
@@ -50,7 +55,7 @@ impl ScrivenerProject {
 }
 
 /// Binder item types
-#[derive(Debug, Deserialize, PartialEq)]
+#[derive(Debug, Default, Deserialize, PartialEq)]
 pub enum BinderItemType {
     /// The single draft folder
     DraftFolder,
@@ -69,6 +74,7 @@ pub enum BinderItemType {
     /// Archived web content
     WebArchive,
     /// Other content type
+    #[default]
     Other,
 }
 
@@ -88,12 +94,6 @@ impl fmt::Display for BinderItemType {
     }
 }
 
-impl Default for BinderItemType {
-    fn default() -> Self {
-        BinderItemType::Other
-    }
-}
-
 /// Deserialise a boolean from Yes / No
 fn de_from_yes_no<'de, D>(deserializer: D) -> Result<bool, D::Error>
 where
@@ -106,6 +106,8 @@ where
 /// Binder item metadata
 #[derive(Debug, Deserialize, PartialEq)]
 pub struct BinderItemMetadata {
+    #[serde(rename = "UUID", default)]
+    pub section_type: Uuid,
     #[serde(rename = "LabelID", default)]
     pub label_id: i32,
     #[serde(rename = "StatusID", default)]
@@ -116,6 +118,51 @@ pub struct BinderItemMetadata {
         default
     )]
     pub include_in_compile: bool,
+    #[serde(rename = "NotesTextSelection", default)]
+    pub notes_text_selection: String,
+    #[serde(rename = "CustomMetaData", default)]
+    pub custom_metadata: Vec<CustomMetadataItem>,
+}
+
+/// Custom metadata items
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename = "MetaDataItem")]
+pub struct CustomMetadataItem {
+    #[serde(rename = "FieldID", default)]
+    pub field_id: String,
+    #[serde(rename = "Value", default)]
+    pub value: String,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct Target {
+    #[serde(rename = "@Type", default)]
+    pub r#type: String,
+    #[serde(rename = "@Notify", default)]
+    pub notify: bool,
+    #[serde(rename = "@ShowOverrun", default)]
+    pub show_overrun: bool,
+    #[serde(rename = "@ShowBuffer", default)]
+    pub show_buffer: bool,
+    #[serde(rename = "$value")]
+    pub value: usize,
+}
+
+/// TextSettings
+///
+/// Contains current selection and any targets set
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct TextSettings {
+    #[serde(rename = "TextSelection", default)]
+    pub text_selection: String,
+    #[serde(rename = "Target", default)]
+    pub target: Option<Target>,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct ItemKeywords {
+    #[serde(rename = "KeywordId", default)]
+    pub keyword_ids: Vec<usize>,
 }
 
 /// A binder item
@@ -123,19 +170,25 @@ pub struct BinderItemMetadata {
 /// Maybe folder, text or other content
 #[derive(Debug, Deserialize, PartialEq)]
 pub struct BinderItem {
-    #[serde(rename = "UUID", default)]
+    #[serde(rename = "@UUID", default)]
     pub uuid: Uuid,
-    #[serde(rename = "Type", default)]
+    #[serde(rename = "@Type", default)]
     pub r#type: BinderItemType,
     #[serde(rename = "Title", default)]
     pub title: String,
+    #[serde(rename = "MetaData", default)]
+    pub metadata: Option<BinderItemMetadata>,
+    #[serde(rename = "TextSettings", default)]
+    pub text_settings: Option<TextSettings>,
+    #[serde(rename = "Keywords", default)]
+    pub keywords: Option<ItemKeywords>,
     #[serde(rename = "Children", default)]
     pub children: Children,
 }
 
 impl BinderItem {
     /// Iterate over this item and its descendents
-    pub fn iter(&self) -> BinderIterator {
+    pub fn iter(&self) -> BinderIterator<'_> {
         BinderIterator::new_from_root(self)
     }
 }
@@ -149,7 +202,7 @@ pub struct Binder {
 
 impl Binder {
     /// An iterator over all items in the binder
-    pub fn iter(&self) -> BinderIterator {
+    pub fn iter(&self) -> BinderIterator<'_> {
         BinderIterator::new(self.binder_items.iter().collect())
     }
 }
@@ -160,8 +213,64 @@ pub struct Children {
     pub binder_items: Vec<BinderItem>,
 }
 
+#[derive(Debug, Deserialize, PartialEq, Default)]
+pub struct SearchSettings {
+    #[serde(rename = "@Type")]
+    pub r#type: String,
+    #[serde(rename = "@Operator")]
+    pub operator: String,
+    #[serde(rename = "@Scope", default)]
+    pub scope: String,
+    #[serde(rename = "@CaseSensitive")]
+    pub case_sensitive: bool,
+    #[serde(rename = "@IgnoreDiacritics")]
+    pub ignore_diacritics: bool,
+}
+
+#[derive(Debug, Deserialize, PartialEq, Default)]
+pub struct Collection {
+    #[serde(rename = "Title")]
+    pub title: String,
+    #[serde(rename = "@Type")]
+    pub r#type: String,
+    #[serde(rename = "@ID")]
+    pub id: Uuid,
+    #[serde(rename = "@Color")]
+    pub color: String,
+    #[serde(rename = "SearchSettings", default)]
+    pub search_settings: SearchSettings,
+}
+
+/// Configured collections
+#[derive(Debug, Deserialize, PartialEq, Default)]
+pub struct Collections {
+    #[serde(rename = "Collection")]
+    pub collections: Vec<Collection>,
+}
+
+#[derive(Debug, Deserialize, PartialEq, Default)]
+pub struct Keyword {
+    #[serde(rename = "@ID")]
+    pub id: usize,
+    #[serde(rename = "@Color", default)]
+    pub color: String,
+    pub children: ChildKeywords,
+}
+
+#[derive(Debug, Deserialize, PartialEq, Default)]
+pub struct ChildKeywords {
+    pub keywords: Vec<Keyword>,
+}
+
+/// A reference to a binder item with
+pub struct BinderItemLocation<'a> {
+    pub item: &'a BinderItem,
+    pub parents: Vec<&'a BinderItem>,
+}
+
 /// An iterator over binder items
 pub struct BinderIterator<'a> {
+    root_queue: VecDeque<&'a BinderItem>,
     stack: Vec<&'a BinderItem>,
 }
 
@@ -169,26 +278,56 @@ impl<'a> BinderIterator<'a> {
     /// An iterator over the binder's items
     pub fn new(roots: Vec<&'a BinderItem>) -> BinderIterator<'a> {
         BinderIterator {
-            stack: roots.into_iter().rev().collect(),
+            root_queue: roots.into_iter().collect(),
+            stack: vec![],
         }
     }
 
     pub fn new_from_root(root: &'a BinderItem) -> BinderIterator<'a> {
-        BinderIterator { stack: vec![root] }
+        BinderIterator {
+            root_queue: VecDeque::from([root]),
+            stack: vec![],
+        }
     }
 }
 
+// impl<'a> Iterator for BinderIterator<'a> {
+//     type Item = &'a BinderItem;
+
+//     fn next(&mut self) -> Option<Self::Item> {
+//         if let Some(item) = self.stack.pop() {
+//             if !item.children.binder_items.is_empty() {
+//                 self.stack.extend(item.children.binder_items.iter().rev());
+//                 Some(item)
+//             } else {
+//                 Some(item)
+//             }
+//         } else {
+//             None
+//         }
+//     }
+// }
+
 impl<'a> Iterator for BinderIterator<'a> {
-    type Item = &'a BinderItem;
+    type Item = BinderItemLocation<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(item) = self.stack.pop() {
+        if let Some(item) = self.stack.last().copied() {
             if !item.children.binder_items.is_empty() {
                 self.stack.extend(item.children.binder_items.iter().rev());
-                Some(item)
+                let parents = self.stack.clone();
+                Some(BinderItemLocation { item, parents })
             } else {
-                Some(item)
+                let parents = self.stack.clone();
+                self.stack.pop();
+                Some(BinderItemLocation { item, parents })
             }
+        } else if let Some(item) = self.root_queue.pop_front() {
+            self.stack.push(item);
+            Some(BinderItemLocation {
+                item,
+                parents: vec![],
+            })
         } else {
             None
         }
